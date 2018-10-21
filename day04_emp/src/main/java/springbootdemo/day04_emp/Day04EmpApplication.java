6e3f708549b8dbadcc91709af068db0c6f0b9abd
ghzdw <zhaodwaiguoxue@163.com>
2018-10-21 12:19:32 +0800

    restful员工管理案例

---
+++ day04_emp/src/main/java/springbootdemo/day04_emp/Day04EmpApplication.java
@@ -0,0 +1,12 @@
+package springbootdemo.day04_emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day04EmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day04EmpApplication.class, args);
+	}
+}
